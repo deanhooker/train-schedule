619395cd46fe3952de26ddfffd48b2e53c72c0ca
Dean Hooker <deanhooker91@gmail.com>
2018-01-06 07:33:23 -0700

    firebase fully functional

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,8 +9,8 @@ var config = {
 };
 firebase.initializeApp(config);
 
+//create global variables
 const database = firebase.database();
-
 let trainName;
 let trainDestination;
 let firstTrainTime;
@@ -18,27 +18,50 @@ let trainFrequency;
 let nextArrival;
 let minutesAway;
 
-$('#submitBtn').on('click', function(event) {
+//on submit click push data to firebase
+$('#submitBtn').on('click', function (event) {
+
+    //prevent default submit function
     event.preventDefault();
 
+    //set local variables
     trainName = $('#train-name').val().trim();
     trainDestination = $('#train-destination').val().trim();
     firstTrainTime = $('#first-train-time').val().trim();
     trainFrequency = $('#train-frequency').val().trim();
-  
+
+    //push to local variables to firebase
     database.ref().push({
-      name: trainName,
-      destination: trainDestination,
-      firstTrain: firstTrainTime,
-      frequency: trainFrequency
+        name: trainName,
+        destination: trainDestination,
+        firstTrain: firstTrainTime,
+        frequency: trainFrequency
     });
-  
+
+    //clear entry form
     $('#train-name').val('');
     $('#train-destination').val('');
     $('#first-train-time').val('');
     $('#train-frequency').val('');
+});
 
-    console.log(trainName);
-  
-  });
-  
+//update schedule table whenever data is added to firebase
+database.ref().on('child_added', function (childSnapshot) {
+
+    console.log(childSnapshot.val());
+
+    //pull firebase variables to set local variables
+    trainName = childSnapshot.val().name;
+    trainDestination = childSnapshot.val().destination;
+    firstTrainTime = childSnapshot.val().firstTrain;
+    trainFrequency = childSnapshot.val().frequency;
+
+    //test values
+    nextArrival = 0;
+    minutesAway = 0;
+
+    //add variables to table
+    $('#train-schedule > tbody').append("<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td>"
+        + trainFrequency + "</td><td>" + nextArrival + "</td><td>" + minutesAway + "</td></tr>");
+
+});
